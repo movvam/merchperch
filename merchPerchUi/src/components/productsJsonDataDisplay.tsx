4f78d659605f4e7c7f5c910e8cc4301e0c11597d
Mani Movva <manikanthmovva@gmail.com>
2025-05-14 19:50:43 +0530

    virtualize products

--- merchPerchUi/src/components/productsJsonDataDisplay.tsx
+++ merchPerchUi/src/components/productsJsonDataDisplay.tsx
@@ -6,6 +6,9 @@ import {
   CardContent
 } from "./ui/card"
 import { cn } from "@/lib/utils"
+import { FixedSizeGrid as Grid } from 'react-window'
+
+const products = JsonData as ProductData[]
 
 type ProductData = {
   product: {
@@ -49,21 +52,21 @@ function Container({
   )
 }
 
-function ProductJsonDataDisplay(){
-	const DisplayData=JsonData.map(
+function ProductCard(){
+	const DisplayData=products.map(
 		(productData: ProductData)=>{
 			return(
         <Container>
             <Card 
             backgroundImage={productData.product.images.edges[0]?.node.url} 
             className="auto-rows-auto  h-60 pb-60 cursor-pointer">
-              {/* <CardContent className="auto-rows-auto	"> */}
+              <CardContent className="auto-rows-auto	">
                 <p className="spotify-product-name-text">
                   {productData.product.title}
                 </p>
-              {/* </CardContent> */}
+              </CardContent>
 
-            {/* </Card> */}/</Card>
+            /</Card>
         </Container>
 			)
 		}
@@ -74,6 +77,43 @@ function ProductJsonDataDisplay(){
 			{DisplayData}
 		</div>
 	)
+}
+
+
+function ProductJsonDataDisplay() {
+  return(
+    <Grid
+  columnCount={3}
+  columnWidth={300}
+  height={800}
+  rowCount={Math.ceil(products.length / 3)}
+  rowHeight={300}
+  width={1000}
+>
+  {({ columnIndex, rowIndex, style }) => {
+    const index = rowIndex * 3 + columnIndex
+    const product = products[index]
+    if (!product) return null
+
+    return (
+      <div style={style}>
+          {/* <Container> */}
+            <Card 
+            backgroundImage={product.product.images.edges[0]?.node.url} 
+            className="auto-rows-auto  h-60 pb-60 cursor-pointer">
+              <CardContent className="auto-rows-auto	">
+                <p className="spotify-product-name-text">
+                  {product.product.title}
+                </p>
+              </CardContent>
+
+            /</Card>
+        {/* </Container> */}
+      </div>
+    )
+  }}
+</Grid>
+  )
 }
 
 export default ProductJsonDataDisplay;
